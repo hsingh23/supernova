ec1ffc6199e61a06e9e05c41e7e3e4d28d796547
Harsh Singh <hsingh@enova.com>
2013-05-22 09:43:47 -0500

    Initial ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supernova::Application.config.secret_token = 'b11f527a622edd152f6a4950878e9d3f3f1cc2347a662a15680c25a4e0a30d814c4c2b4a7116a062e6563aa165b494dceb22791909d6c79b06f5ad51a7c00c0c'
